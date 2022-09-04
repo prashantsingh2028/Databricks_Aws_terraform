5b536e9097fa38ba17925caa32010026a0e8a288
prashantsingh <prashantsingh2028@gmail.com>
2022-09-04 15:48:53 +0800

    commit on

---
+++ init.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    databricks = {
+      source  = "databricks/databricks"
+      version = "1.0.0"
+    }
+    aws = {
+      source = "hashicorp/aws"
+      version = "3.49.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+  access_key = "AKIAW4GN5GZLGBCX4IFW"
+  secret_key = "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"
+}
+
+// Initialize provider in "MWS" mode to provision the new workspace.
+// alias = "mws" instructs Databricks to connect to https://accounts.cloud.databricks.com, to create
+// a Databricks workspace that uses the E2 version of the Databricks on AWS platform.
+// See https://registry.terraform.io/providers/databricks/databricks/latest/docs#authentication
+provider "databricks" {
+  alias    = "mws"
+  host     = "https://accounts.cloud.databricks.com"
+  username = var.databricks_account_username
+  password = var.databricks_account_password
+}
